dfee623ea9283acde22914e79332a75d48c2ef07
slide-t <slideteckacademy@gmail.com>
2025-08-01 21:37:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "essential-kitchen-cache-v8";
+const CACHE_NAME = "essential-kitchen-cache-v9";
 const urlsToCache = [
   "/essentialkitchengear/",
   "/essentialkitchengear/index.html",
